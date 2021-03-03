4b70aa5c88bbdd1977fffff3ba64d704a09df5b2
Roland Salloum <roland.salloum00@outlook.com>
2021-03-03 19:19:38 +0100

    chore: added initial project setup

---
+++ company_service/src/main/java/com/rolandsall/company_service/CompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rolandsall.company_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyServiceApplication.class, args);
+    }
+
+}

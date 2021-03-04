76ed3095cf5baf4f05045a041c11836fbc5a4e6f
Roland Salloum <roland.salloum00@outlook.com>
2021-03-04 07:56:22 +0100

    feat: added config service

---
+++ config_service/src/test/java/com/rolandsall/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolandsall.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

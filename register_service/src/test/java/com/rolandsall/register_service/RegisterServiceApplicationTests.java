08ac0d445bda4f35dcee3f1eab47c5d724bc1221
Roland Salloum <roland.salloum00@outlook.com>
2021-03-10 01:36:49 +0100

    fix: discovery service name and config

---
+++ register_service/src/test/java/com/rolandsall/register_service/RegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolandsall.register_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

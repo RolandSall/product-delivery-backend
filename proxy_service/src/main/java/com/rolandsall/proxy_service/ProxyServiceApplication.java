9a4b1d80bacfae975b0d64eb377e6c7b1faacd22
Roland Salloum <roland.salloum00@outlook.com>
2021-03-11 18:19:37 +0100

    feat: added proxy service

---
+++ proxy_service/src/main/java/com/rolandsall/proxy_service/ProxyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rolandsall.proxy_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyServiceApplication.class, args);
+    }
+
+}
---
+++ proxy_service/src/test/java/com/rolandsall/proxy_service/ProxyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolandsall.proxy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

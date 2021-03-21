eef6edec3586daf70fdd49584719e376fff0dc04
Roland Salloum <roland.salloum00@outlook.com>
2021-03-21 14:48:16 +0100

    chore: added new product service

---
+++ product_service/src/test/java/com/rolandsall/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolandsall.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

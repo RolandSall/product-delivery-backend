ce2a7a76c16435db86957e787ab8b87af86eaf06
Roland Salloum <roland.salloum00@outlook.com>
2021-03-23 22:18:30 +0100

    fix: communication between services

--- distributor_service/src/main/java/com/rolandsall/distributor_service/CompanyService.java
+++ distributor_service/src/main/java/com/rolandsall/distributor_service/CompanyService.java
@@ -21,7 +21,7 @@ public class CompanyService {
     public Company getCompanyInfo(UUID companyId){
         Company company = webClientBuilder.build()
                 .get()
-                .uri("http://company-service/companies/" + companyId)
+                .uri("http://COMPANY-SERVICE/companies/168c4867-1675-479c-bef0-808a24ac383e")
                 .retrieve()
                 .bodyToMono(Company.class)
                 .block();
